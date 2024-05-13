bc5e0fe49cec3d88037cc91692d3e1d73dfbe7fe
unknown <kamblimitesh@gmail.com>
2024-05-14 02:12:05 +0530

    First File Created

---
+++ Array - Copy.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Array{
+	public static void main(String [] args){
+	//int [] arr = new int [ ] {10,20,30};
+	//System.out.println(Arrays.toString(arr));
+	//System.out.println(arr[0] + "\n" +arr[1] +"\n" +arr[2]);
+}//
+
+}
